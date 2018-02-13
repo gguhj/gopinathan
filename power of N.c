62b4aea67a9a44b3be89f5c138788db6a7ad1ea8
gguhj <35652954+gguhj@users.noreply.github.com>
2018-02-13 16:21:38 +0530

    Create power of N.c

---
+++ power of N.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+a=input("Enter the number");
+b=input("number to be raised");
+if(a.isnumeric());
+    a=int(a);
+    if(b.isnumeric();
+        b=int(b);
+        c=a**b;
+        print(c);
+    else:
+        print("The number  entered is invalid");
+else:
+    print("The number  entered is invalid");
+}
+
